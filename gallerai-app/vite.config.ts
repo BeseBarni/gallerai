dff9247d275cdf50a2f828d78d26c5481197e076
BeseBarni <besebarni@gmail.com>
2025-12-28 01:27:05 +0100

    vite config

--- gallerai-app/vite.config.ts
+++ gallerai-app/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     react({
       babel: {
@@ -10,4 +11,16 @@ export default defineConfig({
       },
     }),
   ],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    sourcemap: mode === 'development' ? true : 'hidden',
+  },
+  worker: {
+    format: 'es',
+    sourcemap: true,
+  },
+}))
