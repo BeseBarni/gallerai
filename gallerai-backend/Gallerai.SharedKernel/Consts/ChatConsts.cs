ee753dfe04355bf601d319b584ccd6b78f6ee01a
BeseBarni <besebarni@gmail.com>
2026-02-11 14:59:05 +0100

    prompt suggestions

--- gallerai-backend/Gallerai.SharedKernel/Consts/ChatConsts.cs
+++ gallerai-backend/Gallerai.SharedKernel/Consts/ChatConsts.cs
@@ -2,5 +2,5 @@ namespace Gallerai.SharedKernel.Consts;
 
 public static class ChatConsts
 {
-    public static readonly string UserPrompt = "\"Perform a comprehensive visual analysis of the image. The output must include the final aesthetic score (ranging from 0.0 to 1.0, where 1.0 is the highest) and a detailed critique. Present your full response as a single, valid JSON object containing the following keys: score, critique.\"";
+    public static readonly string UserPrompt = "Perform a comprehensive visual analysis of the image. The output must include the final aesthetic score (ranging from 0.0 to 1.0, where 1.0 is the highest) and a detailed critique. #IMPORTANT!!# Present your full response as a single, valid JSON object containing the following keys: score, detailed_critique.";
 }
