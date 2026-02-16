b18e565750882de06a8dd4d9da3055389004064d
BeseBarni <besebarni@gmail.com>
2026-02-16 16:10:45 +0100

    Remove logs

--- gallerai-backend/Gallerai.SignalR.Shared/Hubs/ImageNotificationsHub.cs
+++ gallerai-backend/Gallerai.SignalR.Shared/Hubs/ImageNotificationsHub.cs
@@ -4,12 +4,4 @@ namespace Gallerai.SignalR.Shared.Hubs;
 
 public class ImageNotificationsHub : Hub
 {
-    public override async Task OnConnectedAsync()
-    {
-        var signalRId = Context.UserIdentifier;
-        Console.WriteLine($"[SignalR] New Connection: {Context.ConnectionId}");
-        Console.WriteLine($"[SignalR] Identity Name: {Context.User?.Identity?.Name}");
-        Console.WriteLine($"[SignalR] UserIdentifier: '{signalRId}'"); // Look for quotes!
-        await base.OnConnectedAsync();
-    }
 }
